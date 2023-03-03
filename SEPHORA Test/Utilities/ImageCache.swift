2277d4df038260c6d980bed7829dbe44e643b570
Moussaab Djeradi <fm_djeradi@esi.dz>
2023-03-03 11:19:06 +0100

    Implement the ImageCache

--- SEPHORA Test/Utilities/ImageCache.swift
+++ SEPHORA Test/Utilities/ImageCache.swift
@@ -18,3 +18,62 @@ protocol ImageCacheType {
         
     subscript(_ url: URL) -> UIImage? { get set }
 }
+
+final class ImageCache {
+    
+    
+    /// The first level cache, that contains encoded images
+    private lazy var imageCache: NSCache<AnyObject, AnyObject> = {
+        let cache = NSCache<AnyObject, AnyObject>()
+        return cache
+    }()
+    /// The second level cache, that contains decoded images
+    private lazy var decodedImageCache: NSCache<AnyObject, AnyObject> = {
+        let cache = NSCache<AnyObject, AnyObject>()
+        return cache
+    }()
+    
+    private let lock = NSLock()
+}
+
+extension ImageCache: ImageCacheType {
+    
+    func image(for url: URL) -> UIImage? {
+        lock.lock(); defer { lock.unlock() }
+        /// search for decoded image
+        if let decodedImage = decodedImageCache.object(forKey: url as AnyObject) as? UIImage {
+            return decodedImage
+        }
+        /// search for image data
+        if let image = imageCache.object(forKey: url as AnyObject) as? UIImage {
+            let decodedImage = image.decodedImage()
+            decodedImageCache.setObject(image as AnyObject, forKey: url as AnyObject)
+            return decodedImage
+        }
+        return nil
+    }
+    
+    func insertImage(_ image: UIImage?, for url: URL) {
+        guard let image = image else { return removeImage(for: url) }
+        let decodedImage = image.decodedImage()
+        
+        lock.lock(); defer { lock.unlock() }
+        imageCache.setObject(image, forKey: url as AnyObject)
+        decodedImageCache.setObject(decodedImage as AnyObject, forKey: url as AnyObject)
+    }
+    
+    func removeImage(for url: URL) {
+        lock.lock(); defer { lock.unlock() }
+        imageCache.removeObject(forKey: url as AnyObject)
+        decodedImageCache.removeObject(forKey: url as AnyObject)
+    }
+    
+    subscript(_ key: URL) -> UIImage? {
+        get {
+            return image(for: key)
+        }
+        set {
+            return insertImage(newValue, for: key)
+        }
+    }
+}
